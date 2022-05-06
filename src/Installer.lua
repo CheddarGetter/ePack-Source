34bb03dd752710d91cb51579115d5534f337aaa2
CheddarGetter <54738810+CheddarGetter@users.noreply.github.com>
2022-05-06 16:00:47 -0700

    Changed some ui things

--- src/Installer.lua
+++ src/Installer.lua
@@ -1,55 +1,38 @@
--- Instances
-local ePackPrompt = Instance.new("ScreenGui")
+local Prompt = Instance.new("ScreenGui")
+Prompt.Name = "Prompt"
+Prompt.DisplayOrder = 1e+09
+Prompt.IgnoreGuiInset = true
+Prompt.ResetOnSpawn = false
+Prompt.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
 local BackFrame = Instance.new("Frame")
-local Layout = Instance.new("UIListLayout")
-local Title = Instance.new("TextLabel")
-local Padding = Instance.new("UIPadding")
-local InfoLabel = Instance.new("TextLabel")
-local ButtonHolder = Instance.new("Frame")
-local ButtonPrefab = Instance.new("TextButton")
-local Corner = Instance.new("UICorner")
-local Stroke = Instance.new("UIStroke")
-local ButtonPadding = Instance.new("UIPadding")
-local UIListLayout = Instance.new("UIListLayout")
-local Stroke_2 = Instance.new("UIStroke")
-local Corner_2 = Instance.new("UICorner")
-
--- Instance Properties
-ePackPrompt.Name = "ePackPrompt"
-ePackPrompt.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-ePackPrompt.DisplayOrder = 1000000000
-ePackPrompt.ResetOnSpawn = false
-if (syn) then syn.protect_gui(ePackPrompt) end
-ePackPrompt.Parent = syn and game:GetService("CoreGui") or game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
-
 BackFrame.Name = "BackFrame"
-BackFrame.AutomaticSize = Enum.AutomaticSize.XY
 BackFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+BackFrame.AutomaticSize = Enum.AutomaticSize.XY
 BackFrame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-BackFrame.BackgroundTransparency = 0.050
 BackFrame.BorderColor3 = Color3.fromRGB(255, 255, 255)
 BackFrame.BorderSizePixel = 0
-BackFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
-BackFrame.Parent = ePackPrompt
+BackFrame.Position = UDim2.fromScale(0.5, 0.5)
 
+local Stroke = Instance.new("UIStroke")
+Stroke.Name = "Stroke"
+Stroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+Stroke.Color = Color3.fromRGB(220, 220, 220)
+Stroke.Parent = BackFrame
+
+local Corner = Instance.new("UICorner")
+Corner.Name = "Corner"
+Corner.CornerRadius = UDim.new(0, 4)
+Corner.Parent = BackFrame
+
+local Layout = Instance.new("UIListLayout")
 Layout.Name = "Layout"
+Layout.Padding = UDim.new(0, 10)
 Layout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 Layout.SortOrder = Enum.SortOrder.LayoutOrder
-Layout.Padding = UDim.new(0, 25)
 Layout.Parent = BackFrame
 
-Title.Name = "Title"
-Title.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Title.BackgroundTransparency = 1.000
-Title.Size = UDim2.new(0, 300, 0, 25)
-Title.RichText = true
-Title.Font = Enum.Font.ArialBold
-Title.Text = "Text"
-Title.TextColor3 = Color3.fromRGB(220, 220, 220)
-Title.TextSize = 23.000
-Title.TextYAlignment = Enum.TextYAlignment.Top
-Title.Parent = BackFrame
-
+local Padding = Instance.new("UIPadding")
 Padding.Name = "Padding"
 Padding.PaddingBottom = UDim.new(0, 10)
 Padding.PaddingLeft = UDim.new(0, 10)
@@ -57,52 +40,69 @@ Padding.PaddingRight = UDim.new(0, 10)
 Padding.PaddingTop = UDim.new(0, 10)
 Padding.Parent = BackFrame
 
+local Title = Instance.new("TextLabel")
+Title.Name = "Title"
+Title.Font = Enum.Font.ArialBold
+Title.RichText = true
+Title.Text = "Text"
+Title.TextColor3 = Color3.fromRGB(220, 220, 220)
+Title.TextSize = 23
+Title.TextYAlignment = Enum.TextYAlignment.Top
+Title.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Title.BackgroundTransparency = 1
+Title.Size = UDim2.fromOffset(300, 25)
+Title.Parent = BackFrame
+
+local InfoLabel = Instance.new("TextLabel")
 InfoLabel.Name = "InfoLabel"
-InfoLabel.AutomaticSize = Enum.AutomaticSize.XY
-InfoLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-InfoLabel.BackgroundTransparency = 1.000
-InfoLabel.RichText = true
 InfoLabel.Font = Enum.Font.SourceSans
+InfoLabel.RichText = true
 InfoLabel.Text = "Text"
 InfoLabel.TextColor3 = Color3.fromRGB(220, 220, 220)
-InfoLabel.TextSize = 23.000
-InfoLabel.TextTransparency = 0.250
+InfoLabel.TextSize = 23
+InfoLabel.TextTransparency = 0.25
 InfoLabel.TextWrapped = true
 InfoLabel.TextYAlignment = Enum.TextYAlignment.Top
+InfoLabel.AutomaticSize = Enum.AutomaticSize.XY
+InfoLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+InfoLabel.BackgroundTransparency = 1
 InfoLabel.Parent = BackFrame
 
+local ButtonHolder = Instance.new("Frame")
 ButtonHolder.Name = "ButtonHolder"
 ButtonHolder.AutomaticSize = Enum.AutomaticSize.X
 ButtonHolder.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-ButtonHolder.BackgroundTransparency = 1.000
-ButtonHolder.Size = UDim2.new(0, 300, 0, 35)
+ButtonHolder.BackgroundTransparency = 1
+ButtonHolder.Size = UDim2.fromOffset(300, 35)
 ButtonHolder.Visible = false
-ButtonHolder.Parent = BackFrame
 
+local ButtonPrefab = Instance.new("TextButton")
 ButtonPrefab.Name = "ButtonPrefab"
+ButtonPrefab.Font = Enum.Font.SourceSans
+ButtonPrefab.RichText = true
+ButtonPrefab.Text = "Text"
+ButtonPrefab.TextColor3 = Color3.fromRGB(220, 220, 220)
+ButtonPrefab.TextSize = 23
 ButtonPrefab.AutomaticSize = Enum.AutomaticSize.X
 ButtonPrefab.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-ButtonPrefab.BackgroundTransparency = 0.600
+ButtonPrefab.BackgroundTransparency = 0.6
 ButtonPrefab.BorderSizePixel = 0
 ButtonPrefab.Size = UDim2.new(0, 150, 1, 0)
 ButtonPrefab.Visible = false
-ButtonPrefab.RichText = true
-ButtonPrefab.Font = Enum.Font.SourceSans
-ButtonPrefab.Text = "Text"
-ButtonPrefab.TextColor3 = Color3.fromRGB(220, 220, 220)
-ButtonPrefab.TextSize = 23.000
-ButtonPrefab.Parent = ButtonHolder
 
-Corner.Name = "Corner"
-Corner.CornerRadius = UDim.new(0, 4)
-Corner.Parent = ButtonPrefab
+local Corner1 = Instance.new("UICorner")
+Corner1.Name = "Corner1"
+Corner1.CornerRadius = UDim.new(0, 4)
+Corner1.Parent = ButtonPrefab
 
-Stroke.Name = "Stroke"
-Stroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-Stroke.Color = Color3.fromRGB(115, 115, 115)
-Stroke.Thickness = 0.800
-Stroke.Parent = ButtonPrefab
+local Stroke1 = Instance.new("UIStroke")
+Stroke1.Name = "Stroke1"
+Stroke1.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+Stroke1.Color = Color3.fromRGB(115, 115, 115)
+Stroke1.Thickness = 0.8
+Stroke1.Parent = ButtonPrefab
 
+local ButtonPadding = Instance.new("UIPadding")
 ButtonPadding.Name = "ButtonPadding"
 ButtonPadding.PaddingBottom = UDim.new(0, 3)
 ButtonPadding.PaddingLeft = UDim.new(0, 3)
@@ -110,22 +110,27 @@ ButtonPadding.PaddingRight = UDim.new(0, 3)
 ButtonPadding.PaddingTop = UDim.new(0, 3)
 ButtonPadding.Parent = ButtonPrefab
 
+ButtonPrefab.Parent = ButtonHolder
+
+local UIListLayout = Instance.new("UIListLayout")
+UIListLayout.Name = "UIListLayout"
+UIListLayout.Padding = UDim.new(0, 10)
 UIListLayout.FillDirection = Enum.FillDirection.Horizontal
 UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
 UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
-UIListLayout.Padding = UDim.new(0, 10)
 UIListLayout.Parent = ButtonHolder
 
-Stroke_2.Name = "Stroke"
-Stroke_2.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-Stroke_2.Color = Color3.fromRGB(255, 255, 255)
-Stroke_2.Transparency = 0.550
-Stroke_2.Parent = BackFrame
+ButtonHolder.Parent = BackFrame
+BackFrame.Parent = Prompt
 
-Corner_2.Name = "Corner"
-Corner_2.CornerRadius = UDim.new(0, 4)
-Corner_2.Parent = BackFrame
+
+-- Non convertable
+if (syn) then
+    syn.protect_gui(Prompt)
+end
+
+Prompt.Parent = syn and game:GetService("CoreGui") or game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
 
 
 -- Functions
@@ -165,7 +170,7 @@ local function CreateCloseButton()
     DestroyButtons()
 
     NewButton("Close", function()
-        ePackPrompt:Destroy()
+        Prompt:Destroy()
 
         if (InstallCompleted) then -- for debugging lol
             InstallCompleted:Fire()
@@ -174,6 +179,7 @@ local function CreateCloseButton()
 end
 
 
+-- Functionality
 SetTitleText("ePack")
 SetInfoText("Would you like to install ePack?\nThis creates files & folders inside of your workspace folder")
 
@@ -209,5 +215,5 @@ NewButton("Yes", function()
 end)
 
 NewButton("No", function()
-    ePackPrompt:Destroy()
+    Prompt:Destroy()
 end)
